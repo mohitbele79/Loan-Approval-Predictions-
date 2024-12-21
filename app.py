8a0b463fee977eb543cdda4336e334c102befb1f
mohitbele79 <mohitbele79@gmail.com>
2024-12-21 18:02:17 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -139,5 +139,5 @@ def batch_prediction():
 
 
 if __name__=='__main__':
-    app.run(debug=True)
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
